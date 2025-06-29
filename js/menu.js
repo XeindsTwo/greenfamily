020feeb9e590b05c6bcbed65fb54e7d52fa34998
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2025-06-29 19:32:01 +0300

    hotfix: - добавлено описание для сайта - добавлен скрол к секциям для ссылок мобильного меню при клике

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,83 @@
-const menuBtn = document.querySelector('.menu-btn');
-const mobileMenu = document.querySelector('.mobile-menu');
-const html = document.documentElement;
-const body = document.body;
+document.addEventListener('DOMContentLoaded', function () {
+  const html = document.documentElement;
+  const body = document.body;
+  const menuBtn = document.querySelector('.menu-btn');
+  const headerMobile = document.querySelector('.mobile-menu');
+  const anchors = document.querySelectorAll('.header__link, .header__link.mobile');
+  const allFocusableElements = document.querySelectorAll('a, button, input, textarea, select, [tabindex]');
 
-menuBtn.addEventListener('click', function () {
-  this.classList.toggle('active');
-  mobileMenu.classList.toggle('active');
+  function toggleMenu() {
+    const isActive = menuBtn.classList.toggle('active');
+    headerMobile.classList.toggle('active');
+    html.classList.toggle('active');
 
-  const isActive = this.classList.contains('active');
+    if (isActive) {
+      // блокируем скрол при активном меню
+      html.style.overflow = 'hidden';
+      body.style.overflow = 'hidden';
 
-  if (isActive) {
-    html.style.overflowX = 'hidden';
-    body.style.overflowX = 'hidden';
-    body.style.overflowY = 'hidden';
-    html.style.overflowY = 'hidden';
-  } else {
-    html.style.overflowX = '';
-    html.style.overflowY = '';
-    body.style.overflowX = '';
-    body.style.overflowY = '';
+      // делает только нужные элементы доступными
+      anchors.forEach(anchor => anchor.removeAttribute('tabindex'));
+      allFocusableElements.forEach(element => {
+        if (!headerMobile.contains(element) && element !== menuBtn) {
+          element.setAttribute('tabindex', '-1');
+        }
+      });
+    } else {
+      // восстановить прокрутку
+      html.style.overflow = '';
+      body.style.overflow = '';
+
+      // вернуть доступность всем
+      anchors.forEach(anchor => anchor.setAttribute('tabindex', '-1'));
+      allFocusableElements.forEach(element => {
+        element.removeAttribute('tabindex');
+      });
+    }
+
+    menuBtn.blur(); // убрать фокус с кнопки
+  }
+
+  function scrollToTarget(targetId) {
+    const targetSection = document.querySelector(targetId);
+    if (targetSection) {
+      html.classList.remove('active');
+      headerMobile.classList.remove('active');
+      menuBtn.classList.remove('active');
+
+      // восстановление исходного значения overflow, когда идёт скролл к меню
+      html.style.overflow = '';
+      body.style.overflow = '';
+      anchors.forEach(anchor => anchor.setAttribute('tabindex', '-1'));
+      allFocusableElements.forEach(element => {
+        element.removeAttribute('tabindex');
+      });
+
+      setTimeout(() => {
+        const targetOffset = targetSection.offsetTop - 25;
+        window.scrollTo({top: targetOffset, behavior: 'smooth'});
+      }, 700);
+    }
+  }
+
+  function handleAnchorClick(event) {
+    if (this.classList.contains('mobile')) {
+      event.preventDefault();
+      const href = this.getAttribute('href');
+      const [, anchorId] = href.split('#');
+      if (anchorId) {
+        scrollToTarget('#' + anchorId);
+      }
+    }
+  }
+
+  // привязка событий к мобильному меню и кнопке
+  if (menuBtn && headerMobile) {
+    menuBtn.addEventListener('click', toggleMenu);
+  }
+
+  for (const anchor of anchors) {
+    anchor.addEventListener('click', handleAnchorClick);
+    anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
   }
 });
